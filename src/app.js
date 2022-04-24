d5076f13a31f90b4798609f4285ff1e4f86450d7
himanshu khare <himanshu@wordpressindia.in>
2022-04-24 19:38:44 +0530

    Init Commit

---
+++ src/app.js
@@ -0,0 +1,38 @@
+const path=require('path');
+const express=require('express');
+const hbs=require('hbs');
+const app=express();
+
+const publicDirPath=path.join(__dirname,'../public');
+const layoutDirPaths=path.join(__dirname,'../views/layouts');
+app.set("views", path.join(__dirname,'../views'));
+hbs.registerPartials(layoutDirPaths);
+
+app.set("view engine", "hbs");
+app.use(express.static(publicDirPath));
+
+app.get('',(req, res) =>{
+    res.render('index',{
+        'title':'Test HBS',
+        'name':'Him'
+    });
+});
+
+
+app.get('/about',(req, res) =>{
+    res.render('index',{
+        'title':'About Us',
+        'name':'My name is Him'
+    });
+});
+
+app.get('/weather',(req, res) =>{
+    res.render('index',{
+        'title':'Test Weather HBS',
+        'name':'Himalaya Weather is very cold'
+    });
+});
+
+app.listen(3000, () => {
+console.log('Server is up on port 3000');
+});
